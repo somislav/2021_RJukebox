567f9a548b7f86219555dd1ff2c7ba85d224a588
SpotRZ <spot.rusherz96@gmail.com>
2021-05-04 08:17:28 +0200

    Expanding /api/songs to accept parameters (#16)

--- api/README.md
+++ api/README.md
@@ -4,8 +4,41 @@ Each API method __needs to start with /api/__<br>
 
 
 ## GET Endpoints
-- `/api/songs` - Returns list of all songs and respective info.
 
+```
+GET /api/songs
+```
+
+Returns list of all songs and respective info.
+
+| Parameter         | Value                                                             |
+| :---------------- | :-----------------------------------------------------------------|
+| *song_name*       | Filter by the name of the song                                    |
+| *artist*          | Filter by artist name                                             |
+| *genre*           | Filter by genre. Acceptable values: Rock, HipHop, Metal, Pop, RnB |
+| *yt_link*         | Filter by youtube link, *include complete link*.                  |
+
+<br>
+
+| Return code       | Explanation          |
+| :---------------- | :------------------- |
+| *200..299*        | Successful request   |
+| *420*             | Bad Parameter        |
+| *430*             | Unprocessable entity |
+
+<br>
+
+```python
+# PYTHON EXAMPLE
+import requests
+
+params = { 'song_name': 'Runaway', 'artist': 'Kanye West' }
+
+response = requests.get('<base_url>/api/songs', params=params)
+response.raise_for_status()
+
+print(response.json())
+```
 
 ## Folder content
 * __api_getter.py__
--- api/api_getter.py
+++ api/api_getter.py
@@ -3,18 +3,33 @@ import logging
 from flask import Blueprint
 from flask import make_response
 from flask import jsonify
+from flask import request
 
 from utilities.db_util import connect_execute_query
 
 
 api_getter = Blueprint('api_getter', __name__)
 
+song_params = ['song_name', 'artist', 'genre', 'yt_link']
+
 
 @api_getter.route('/api/songs')
 def get_songs():
-    logging.info("User requested list of all songs.")
+    logging.info("User requested list of songs.")
     try:
-        query = "SELECT * FROM songs"
+        if invalid_parameters():
+            logging.info("Bad parameter added to request.")
+            return make_response(f"Bad parameter", 420)
+
+        where_query = ''
+        for param in song_params:
+            if param in request.args:
+                logging.info(f"User requested {param} to be {request.args.get(param)}.")
+                where_query += f"{' AND' if where_query else ''} {param}='{request.args.get(param)}'"
+
+        where_query = f" WHERE{where_query}" if where_query else ''
+
+        query = f"SELECT * FROM songs{where_query}"
         songs = connect_execute_query(query)
         response = [ { 'song_name': song[1], 'artist': song[2], 'genre': song[3], 'yt_link': song[4] } for song in songs ]
 
@@ -22,3 +37,7 @@ def get_songs():
     except Exception as e:
         logging.error(f"Error happened while using /api/songs endpoint: {e}")
         return make_response('Could not get the results. Please try later.', 430)
+
+
+def invalid_parameters():
+    return any(arg not in song_params for arg in request.args)
